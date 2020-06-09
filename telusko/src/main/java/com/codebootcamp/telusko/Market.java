96bb55b2e0578ade03eb1650e55757283cdc8ed5
Agrawal <Agrawal@DESKTOP-4JOUJ1N>
2020-06-09 20:16:14 +0530

    MArket file

---
+++ telusko/src/main/java/com/codebootcamp/telusko/Market.java
@@ -0,0 +1,25 @@
+package com.codebootcamp.telusko;
+
+public class Market {
+	
+	private int id;
+	private String name;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Market [id=" + id + ", name=" + name + "]";
+	}
+	
+	
+}
